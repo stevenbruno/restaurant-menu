096678897dd97cb828942c8fa5dfab4cd9585d0a
Steven Bruno <sbruno636@gmail.com>
2020-01-23 21:58:22 -0600

    create item model

---
+++ models/item.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var ItemSchema = new Schema({
+  name: { type: String, required: true, max: 100 },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+  description: { type: String, required: true },
+  price_cents: { type: Number, required: true }
+});
+
+ItemSchema.virtual('url').get(function() {
+  return '/catalog/item/' + this._id;
+});
+
+ItemSchema.virtual('price_usd').get(function() {(this.price_cents/100).toFixed(2)}; 
+
+//Export model
+module.exports = mongoose.model('Item', ItemSchema);
