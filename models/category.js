d7770f62d79fd66e4c0b5638927783b052168b35
Steven Bruno <sbruno636@gmail.com>
2020-01-23 21:58:27 -0600

    create category model

---
+++ models/category.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var CategorySchema = new Schema({
+  name: { type: String, required: true, max: 100 }
+});
+
+CategorySchema.virtual('url').get(function() {
+  return '/catalog/category/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Category', CategorySchema);
