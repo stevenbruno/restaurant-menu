b81f8f1596c883c943d6dd51c987b5519678af84
Steven Bruno <sbruno636@gmail.com>
2020-02-08 11:52:58 -0600

    create category controller

---
+++ controllers/categoryController.js
@@ -0,0 +1,41 @@
+var Category = require('../models/category');
+
+// display home page
+exports.index = function(req, res) {
+  res.send('Restaurant Menu home page');
+};
+
+// Display list of all Categories.
+exports.category_list = function(req, res) {
+  res.send('NOT IMPLEMENTED: Category list');
+};
+
+// Display category create form on GET.
+exports.category_create_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: category create GET');
+};
+
+// Handle category create on POST.
+exports.category_create_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: category create POST');
+};
+
+// Display category delete form on GET.
+exports.category_delete_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: category delete GET');
+};
+
+// Handle category delete on POST.
+exports.category_delete_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: category delete POST');
+};
+
+// Display category update form on GET.
+exports.category_update_get = function(req, res) {
+  res.send('NOT IMPLEMENTED: category update GET');
+};
+
+// Handle category update on POST.
+exports.category_update_post = function(req, res) {
+  res.send('NOT IMPLEMENTED: category update POST');
+};
